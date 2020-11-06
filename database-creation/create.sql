aa36b53fabdb0c749668a23386f70af8f769ca8d
Cezary Grabowski <c.grabowski13@gmail.com>
2020-11-06 16:20:20 +0100

    Dodanie plików tworzenia oraz uzupełnianie wstępnej bazy.

---
+++ database-creation/create.sql
@@ -0,0 +1,143 @@
+--tworzenie bazy danych
+CREATE DATABASE Delp CHARACTER SET utf8 COLLATE utf8_general_ci;	
+
+--tworzenie tabeli Users przechowującej podstawowe dane o uzytkowniku
+CREATE TABLE Users(
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password CHAR(32) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
+    surname VARCHAR(255),
+    birthday DATE,
+    accountCreation DATE,
+    idFirstLanguage INT DEFAULT 1,
+    isBlocked BOOLEAN,
+    userStatus INT,
+    PRIMARY KEY (id)
+);
+
+--tworzenie tabeli ExerciseSets przechowujacej podstawowe informacje o zestawach zadan
+CREATE TABLE ExerciseSets(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    info VARCHAR(1000),
+    idCreator INT NOT NULL,
+    setCreation DATE,
+    idBaseLanguage INT,
+    idLearnLanguage INT,
+    isWordSet BOOLEAN,
+    popularity INT DEFAULT 1,
+    ifVideo BOOLEAN,
+    ifAudio BOOLEAN,
+    ifPicture BOOLEAN,
+    PRIMARY KEY (id)
+);
+
+--tworzenie tabeli SetsExercises by połączyć zestaw z odpowiednim zadaniem 
+CREATE TABLE SetsExercises(
+    id INT NOT NULL AUTO_INCREMENT,
+    idSet INT NOT NULL,
+    idTemplate INT,
+    idExercise INT,
+    PRIMARY KEY (id)
+);
+
+--stworzenie tabeli WordExerciseTemplate by przechować wszystkie zadania tego typu
+CREATE TABLE WordExerciseTemplate(
+    id INT NOT NULL AUTO_INCREMENT,
+    idSet INT NOT NULL,
+    word VARCHAR(255),
+    translation VARCHAR(255),
+    videoPath VARCHAR(255),
+    audioPath VARCHAR(255),
+    picturePath VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+--stworzenie tabeli FillSentanceExerciseTemplate by przechować wszystkie zadania tego typu
+CREATE TABLE FillSentanceExerciseTemplate(
+    id INT NOT NULL AUTO_INCREMENT,
+    idSet INT NOT NULL,
+    leftPartOfSentence VARCHAR(1000),
+    wordToFill VARCHAR(255),
+    rightPartOfSentence VARCHAR(1000),
+    videoPath VARCHAR(255),
+    audioPath VARCHAR(255),
+    picturePath VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+--stworzenie tabeli IncorrectWordsFillSentanceExerciseTemplate by przechować wszystkie dodatkowe słowa do zadania
+CREATE TABLE IncorrectWordsFillSentanceExerciseTemplate(
+    id INT NOT NULL AUTO_INCREMENT,
+    word VARCHAR(255),
+    idFillSentanceExerciseTemplate INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+--stworzenie tabeli TranslateSentanceExerciseTemplate by przechować wszystkie zadania tego typu
+CREATE TABLE TranslateSentanceExerciseTemplate(
+    id INT NOT NULL AUTO_INCREMENT,
+    idSet INT NOT NULL,
+    oryginalSentence VARCHAR(1000),
+    translatedSentence VARCHAR(1000),
+    videoPath VARCHAR(255),
+    audioPath VARCHAR(255),
+    picturePath VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+--tworzenie tabeli FavouriteSets by przechowywać informacje o ulubionych zestawach poszczególnych uzytkownikow
+CREATE TABLE FavouriteSets(
+    id INT NOT NULL AUTO_INCREMENT,
+    idSet INT NOT NULL,
+    idUser INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+--tworzenie tabeli TemplatesInfo przechowującej informacje o rodzajach zadań jakie obsluguje aplikacja
+CREATE TABLE TemplatesInfo(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    info VARCHAR(1000),
+    PRIMARY KEY (id)
+);
+
+--tworzenie tabeli SetsTags przechowującej tagi dla każdego z zestawów
+CREATE TABLE SetsTags(
+    id INT NOT NULL AUTO_INCREMENT,
+    idSet INT NOT NULL,
+    tag VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+--tworzenie tabeli UserPreferences przechowującej tagi dla każdego z zestawów
+CREATE TABLE UserPreferences(
+    id INT NOT NULL AUTO_INCREMENT,
+    idUser INT NOT NULL,
+    idColorSets INT,
+    fontSize INT,
+    noSound BOOLEAN,
+    PRIMARY KEY (id)
+);
+
+--tworzenie tabeli Languages przechowujacej dane o wszystkich mozliwych jezykach do uzycia przez uzytkownikow
+CREATE TABLE Languages(
+    id INT NOT NULL AUTO_INCREMENT,
+    code VARCHAR(50),
+    name VARCHAR(255) NOT NULL,
+    info VARCHAR(1000),
+    PRIMARY KEY (id)
+);
+
+--tworzenie tabeli GuiColors przechowujacej dane o wszystkich trybach kolorystycznych do wybory z bazy(zwykły, bardziej kontrastowy, nocny)
+CREATE TABLE GuiColors(
+    id INT NOT NULL AUTO_INCREMENT,
+    color1 VARCHAR(10),
+    color2 VARCHAR(10),
+    color3 VARCHAR(10),
+    color4 VARCHAR(10),
+    color5 VARCHAR(10),
+    PRIMARY KEY (id)
+);
