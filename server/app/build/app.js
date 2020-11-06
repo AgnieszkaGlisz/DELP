4f013d81673f92b3e0fe4fba13b565d2564b0238
Cezary Grabowski <c.grabowski13@gmail.com>
2020-11-06 15:45:32 +0100

    Dodawanie obslugi sciezki /favourite zwracajacej ulubione zestawy, oraz /languagesinfo/:id zwracajacej informacje o jezyku o danym id

--- server/app/build/app.js
+++ server/app/build/app.js
@@ -19,9 +19,32 @@ app.get('/apiinfo', function (req, res) {
     var info = {
         '/apiinfo': 'Information about api routes.',
         '/login': 'Creates authentication token based on username and password.',
-        '/account': 'Basic user information. Needs authentication token.'
+        '/account': 'Basic user information. Needs authentication token.',
+        '/favourite': 'Favourite wordsets and lessons.',
+        '/languageinfo/:id': 'Information about language based on id.'
     };
     res.json(info);
+});
+//logowanie 
+app.post('/login', function (req, res) {
+    var sql = 'SELECT * FROM Users where username = "' + req.body.username + '" and password = "' + req.body.password + '"';
+    db.query(sql, function (result) {
+        if (result == 0) {
+            res.status(401).json({ error: "Invalid login or password." });
+            return;
+        }
+        if (result[0].isBlocked == true) {
+            common.adminLog('User is blocked.');
+            res.status(403).json({ error: 'User is blocked.' });
+            return;
+        }
+        var userInfo = {
+            id: result[0].id,
+            username: result[0].username
+        };
+        var accessToken = auth.createToken(userInfo);
+        res.json({ accessToken: accessToken });
+    });
 });
 //pobieranie danych uzytkownika
 app.get('/account', auth.authenticateToken, function (req, res) {
@@ -30,11 +53,6 @@ app.get('/account', auth.authenticateToken, function (req, res) {
     db.query(sql, function (result) {
         if (result == 0) {
             res.status(404).json({ error: "User data error." });
-            return;
-        }
-        if (result[0].isBlocked == true) {
-            common.adminLog('User is blocked.');
-            res.status(403).json({ error: 'User is blocked.' });
             return;
         }
         common.adminLog('Preparing and sending user info.');
@@ -58,20 +76,72 @@ app.get('/account', auth.authenticateToken, function (req, res) {
         res.json(userInfo);
     });
 });
-//logowanie 
-app.post('/login', function (req, res) {
-    var sql = 'SELECT * FROM Users where username = "' + req.body.username + '" and password = "' + req.body.password + '"';
+//pobieranie ulubionych Wordsetow
+app.get('/favourite', auth.authenticateToken, function (req, res) {
+    common.adminLog("Favourite sets.");
+    var sql = 'SELECT ExerciseSets.*, Users.username FROM `Users`,`FavouriteSets`,`ExerciseSets` WHERE ExerciseSets.id=FavouriteSets.idSet AND Users.id = FavouriteSets.idUser AND FavouriteSets.idUser = ' + req.user.id;
     db.query(sql, function (result) {
         if (result == 0) {
-            res.status(401).json({ error: "Invalid login or password." });
+            res.status(404).json({ error: "No favourite sets." });
             return;
         }
-        var userInfo = {
-            id: result[0].id,
-            username: result[0].username
+        common.adminLog('Preparing and sending favourite sets.');
+        var wordsets = [];
+        var lessons = [];
+        for (var i = 0; i < result.length; i++) {
+            if (result[i].isWordSet == true)
+                wordsets.push({
+                    id: result[i].id,
+                    name: result[i].name,
+                    info: result[i].info,
+                    idCreator: result[i].idCreator,
+                    usernameCreator: result[i].username,
+                    idBaseLanguage: result[i].idBaseLanguage,
+                    idLearnLanguage: result[i].idLearnLanguage,
+                    ifVideo: result[i].ifVideo,
+                    ifAudio: result[i].ifAudio,
+                    ifPicture: result[i].ifPicture
+                });
+            else
+                lessons.push({
+                    id: result[i].id,
+                    name: result[i].name,
+                    info: result[i].info,
+                    idCreator: result[i].idCreator,
+                    nickCreator: result[i].nickCreator,
+                    idBaseLanguage: result[i].idBaseLanguage,
+                    idLearnLanguage: result[i].idLearnLanguage,
+                    BaseLanguage: result[i].BaseLanguage,
+                    LearnLanguage: result[i].LearnLanguage,
+                    ifVideo: result[i].ifVideo,
+                    ifAudio: result[i].ifAudio,
+                    ifPicture: result[i].ifPicture
+                });
+        }
+        var favourite = {
+            wordsets: wordsets,
+            lessons: lessons
         };
-        var accessToken = auth.createToken(userInfo);
-        res.json({ accessToken: accessToken });
+        res.json(favourite);
+    });
+});
+//pobieranie informacji o jezyku
+app.get('/languageinfo/:id', auth.authenticateToken, function (req, res) {
+    common.adminLog("Language info.");
+    var sql = 'SELECT * FROM `Languages` WHERE id = ' + req.params.id;
+    db.query(sql, function (result) {
+        if (result == 0) {
+            res.status(404).json({ error: "No result." });
+            return;
+        }
+        common.adminLog('Preparing and sending language info.');
+        var language = {
+            id: result[0].id,
+            code: result[0].code,
+            name: result[0].name,
+            info: result[0].info
+        };
+        res.json(language);
     });
 });
 //nieobsluzone sciezki
--- server/app/src/app.ts
+++ server/app/src/app.ts
@@ -22,9 +22,33 @@ app.get('/apiinfo', (req, res) => {
     let info = {
         '/apiinfo':'Information about api routes.',
         '/login':'Creates authentication token based on username and password.',
-        '/account':'Basic user information. Needs authentication token.'
+        '/account':'Basic user information. Needs authentication token.',
+        '/favourite':'Favourite wordsets and lessons.',
+        '/languageinfo/:id':'Information about language based on id.'
     }
     res.json(info)
+})
+
+//logowanie 
+app.post('/login', (req, res) => {
+    var sql = 'SELECT * FROM Users where username = "' + req.body.username + '" and password = "' + req.body.password + '"'
+    db.query(sql,function(result:any){
+        if(result == 0){
+            res.status(401).json({error: "Invalid login or password."})
+            return
+        } 
+        if(result[0].isBlocked == true){
+            common.adminLog('User is blocked.')
+            res.status(403).json({error: 'User is blocked.'})
+            return
+        }
+        var userInfo={
+            id: result[0].id,
+            username: result[0].username
+        }
+        const accessToken = auth.createToken(userInfo)
+        res.json({accessToken: accessToken})
+    })
 })
 
 //pobieranie danych uzytkownika
@@ -35,11 +59,6 @@ app.get('/account', auth.authenticateToken, (req:any, res) => {
         if(result == 0){
             res.status(404).json({error: "User data error."})
             return
-        } 
-        if(result[0].isBlocked == true){
-            common.adminLog('User is blocked.')
-            res.status(403).json({error: 'User is blocked.'})
-            return
         }
         common.adminLog('Preparing and sending user info.')
         var userPreferences = {
@@ -47,7 +66,7 @@ app.get('/account', auth.authenticateToken, (req:any, res) => {
             fontSize: result[0].fontSize,
             noSound: result[0].noSound
         }
-        var userInfo ={
+        var userInfo = {
             id: result[0].id,
             username: result[0].username,
             email: result[0].email,
@@ -63,20 +82,73 @@ app.get('/account', auth.authenticateToken, (req:any, res) => {
     })
 })
 
-//logowanie 
-app.post('/login', (req, res) => {
-    var sql = 'SELECT * FROM Users where username = "' + req.body.username + '" and password = "' + req.body.password + '"'
+//pobieranie ulubionych Wordsetow
+app.get('/favourite', auth.authenticateToken, (req:any, res) => {
+    common.adminLog("Favourite sets.")
+    let sql = 'SELECT ExerciseSets.*, Users.username FROM `Users`,`FavouriteSets`,`ExerciseSets` WHERE ExerciseSets.id=FavouriteSets.idSet AND Users.id = FavouriteSets.idUser AND FavouriteSets.idUser = ' + req.user.id; 
     db.query(sql,function(result:any){
         if(result == 0){
-            res.status(401).json({error: "Invalid login or password."})
+            res.status(404).json({error: "No favourite sets."})
             return
         } 
-        var userInfo={
-            id: result[0].id,
-            username: result[0].username
+        common.adminLog('Preparing and sending favourite sets.')
+        var wordsets = []
+        var lessons = []
+        for (var i = 0; i< result.length; i++){
+            if(result[i].isWordSet == true)
+                wordsets.push({
+                    id: result[i].id,
+                    name: result[i].name,
+                    info: result[i].info,
+                    idCreator: result[i].idCreator,
+                    usernameCreator: result[i].username,
+                    idBaseLanguage: result[i].idBaseLanguage,
+                    idLearnLanguage: result[i].idLearnLanguage,
+                    ifVideo: result[i].ifVideo,
+                    ifAudio: result[i].ifAudio,
+                    ifPicture: result[i].ifPicture
+                })
+            else
+                lessons.push({
+                    id: result[i].id,
+                    name: result[i].name,
+                    info: result[i].info,
+                    idCreator: result[i].idCreator,
+                    nickCreator: result[i].nickCreator,
+                    idBaseLanguage: result[i].idBaseLanguage,
+                    idLearnLanguage: result[i].idLearnLanguage,
+                    BaseLanguage: result[i].BaseLanguage,
+                    LearnLanguage: result[i].LearnLanguage,
+                    ifVideo: result[i].ifVideo,
+                    ifAudio: result[i].ifAudio,
+                    ifPicture: result[i].ifPicture
+                })     
         }
-        const accessToken = auth.createToken(userInfo)
-        res.json({accessToken: accessToken})
+        var favourite = {
+            wordsets: wordsets,
+            lessons: lessons
+        }
+        res.json(favourite)
+    })
+})
+
+//pobieranie informacji o jezyku
+app.get('/languageinfo/:id', auth.authenticateToken, (req:any, res) => {
+    common.adminLog("Language info.")
+    let sql = 'SELECT * FROM `Languages` WHERE id = ' + req.params.id
+    db.query(sql,function(result:any){
+        if(result == 0){
+            res.status(404).json({error: "No result."})
+            return
+        } 
+        common.adminLog('Preparing and sending language info.')
+        var language = {
+            id: result[0].id,
+            code: result[0].code,
+            name: result[0].name,
+            info: result[0].info
+        }
+        res.json(language)
     })
 })
 
