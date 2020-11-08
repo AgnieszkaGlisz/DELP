8bc7e9ef88c2b2ba546b257afd596b41487a01e2
Cezary Grabowski <c.grabowski13@gmail.com>
2020-11-08 13:14:28 +0100

    gitignore

--- server/app/build/common.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.adminLog = void 0;
-function adminLog(message) {
-    if (process.env.ADMIN == "admin")
-        console.log(message);
-}
-exports.adminLog = adminLog;
