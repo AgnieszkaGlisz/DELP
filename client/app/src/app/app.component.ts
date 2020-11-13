a0185bffba5c3be0a292ec3d2c77a46427d6d779
VoidQ <wojtek@krzemdev.pl>
2020-11-13 15:35:33 +0100

    hide menu

--- client/app/src/app/app.component.ts
+++ client/app/src/app/app.component.ts
@@ -8,11 +8,24 @@ import { UserService } from './_services/user.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  implements OnInit {
+
   constructor(
     private userService: UserService,
     private router: Router,
-    ) { }
-
+    ) {
+      this.router.events.subscribe( x => {
+        if (this.router.url.indexOf('login') > -1) 
+        {
+          this.logged = false;
+        }
+        else
+        {
+          this.logged = true;
+        }
+      }) 
+    }
+  
+  logged: boolean = true;
   title = 'lang-app-front';
 
   ngOnInit(): void {
