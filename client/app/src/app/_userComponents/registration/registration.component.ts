d7a25734a67570bef9f1583f0afbb98537bf8703
VoidQ <wojtek@krzemdev.pl>
2020-12-04 12:02:38 +0100

    languages register

--- client/app/src/app/_userComponents/registration/registration.component.ts
+++ client/app/src/app/_userComponents/registration/registration.component.ts
@@ -1,30 +1,35 @@
 import { Language } from './../../_interfaces/language';
 import { User } from './../../_interfaces/user';
 import { UserService } from './../../_services/user.service';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnInit } from '@angular/core';
 import { UserPreferences } from '../../_interfaces/userPreferences';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserInfo } from '../../_interfaces/userInfo';
+import { Subscription } from 'rxjs';
+import { WordsetService } from '../../_services/wordset.service';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent implements OnInit {
+export class RegistrationComponent implements OnInit, AfterViewInit, AfterViewChecked {
 
   constructor(
     public userService: UserService,
     private formBuilder: FormBuilder,
     private router: Router,
+    private wordsetService: WordsetService, 
     ) { }
 
   user: User;
   registerForm: FormGroup;
   submitted = false;
-  languageList: Array<Language> = new Array<Language>();
 
+  lang1 = new FormControl();
+  languageList: Array<Language> = new Array<Language>();
+  languageSubscription: Subscription;
 
   ngOnInit(): void {
     this.user = new User();
@@ -50,6 +55,47 @@ export class RegistrationComponent implements OnInit {
   });
   }
 
+  ngAfterViewInit(): void {
+    this.loadLanguages();
+  }
+
+  ngAfterViewChecked(): void {
+    if (this.languageList.length == 0 && ((!this.languageSubscription) ||
+       (this.languageSubscription && this.languageSubscription.closed)))
+      this.loadLanguages();
+  }
+
+  loadLanguages(): void {
+    console.log("waiting for languages...");
+    this.languageSubscription = this.wordsetService.getAllLanguages().subscribe( x => {
+      console.log("got languages", x);
+      let tmp = {languages:  Array<Language>()};
+      Object.assign(tmp, x);
+      let i = 0;
+      tmp.languages.forEach( l =>
+      {
+        this.languageList[i] = l;
+        i++;
+      });
+    }, err => {
+      console.log("didn't get languages", err);
+      let tmpList = new Array<Language>();
+      tmpList[0] = {code: "PL",
+      id: 1,
+      info: "PLinfo",
+      name: "Polski"};
+      tmpList[1] = {code: "EN",
+      id: 2,
+      info: "ENinfo",
+      name: "Angielski"};
+      Object.assign(this.languageList, tmpList);
+    }, 
+    () => {
+      console.log("languages", this.languageList);
+    }
+    )
+  }
+
   get f() { return this.registerForm.controls; }
 
   onSubmit() {
@@ -57,10 +103,10 @@ export class RegistrationComponent implements OnInit {
     // stop here if form is invalid
     this.writeFormDataInUser();
     if (this.registerForm.invalid) {
-      //return;
+      return;
     }
     
-    this.writeTestDataInUser();
+    //this.writeTestDataInUser();
     this.userService.sendRegistrationInfo(this.user).subscribe();
     // alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
   }
@@ -83,7 +129,7 @@ export class RegistrationComponent implements OnInit {
     this.user.userInfo.surname = this.registerForm.value.lastName;
     this.user.userInfo.email = this.registerForm.value.email;
     this.user.userInfo.password = this.registerForm.value.password;
-    this.user.userInfo.idFirstLanguage = 1;
+    this.user.userInfo.idFirstLanguage = this.lang1.value.id;;
     this.user.userInfo.birthday = this.registerForm.value.birthday;
     this.user.userInfo.username = this.registerForm.value.username;
     this.user.preferences.noSound = this.registerForm.value.hearingDisability;
