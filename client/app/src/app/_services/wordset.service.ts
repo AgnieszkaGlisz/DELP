a6609b2448039abcfba911456ff0162d9055379a
blejs13 <c.grabowski13@gmail.com>
2020-12-19 18:53:37 +0100

    xd

--- client/app/src/app/_services/wordset.service.ts
+++ client/app/src/app/_services/wordset.service.ts
@@ -21,7 +21,7 @@ export class WordsetService {
   urlAga: string = `http://25.95.136.77:3500`;
   urlCezar: string = `http://25.68.211.177:3500`;
   urlLocal: string = `http://localhost:3500`;
-  url: string = this.urlAga;
+  url: string = this.urlLocal;
   setToDisplayId: string = '0';
 
   correctExercises: number;
